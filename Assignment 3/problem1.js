f6771825351918faf3ffbecdb9b374ee094fe28b
JaredAlexSolo <jasolomon1@aggies.ncat.edu>
2019-10-24 23:21:50 -0400

    ew

--- Assignment 3/problem1.js
+++ Assignment 3/problem1.js
@@ -4,12 +4,13 @@ function loadName() {
   var names = [];
   var scores = [];
 
-  while ( x = window.prompt( "Student's name Cancel to Exit", 0 ) )  {
+  while ( x = window.prompt( "Student's name Cancel to Exit", 0 ) ) {
     names.push(x);
 
     do{
       y = window.prompt( "Student score(between "+minScore+" and "+maxScore+")", 0 );
     }while(isNaN(y) || y > 10 || y < 0);
+    scores.push(parseInt(y));
 }
 
 
