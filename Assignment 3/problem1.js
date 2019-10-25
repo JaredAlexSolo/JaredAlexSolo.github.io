651def39979e8add8b8c190356725d0ed1a26ecb
JaredAlexSolo <jasolomon1@aggies.ncat.edu>
2019-10-24 22:40:32 -0400

    sdfggdsfggdfsdd

--- Assignment 3/problem1.js
+++ Assignment 3/problem1.js
@@ -2,10 +2,11 @@ function loadName() {
   var names = [];
   var scores = [];
 
-  while ( x = window.prompt( "Value of x", 0 ) )  {
+  while ( x = window.prompt( "Student's name Cancel to Exit", 0 ) )  {
     document.getElementById('paragraph').innerHTML = "hey";
     names.push(parseInt(x));
-    y = window.prompt( "Value of y", 0 );
+    y = window.prompt( "Student score(between 0 and 10)", 0 );
+    scores.push(y);
   }
-  document.getElementById('paragraph').innerHTML = scores;
+  document.getElementById('paragraph').innerHTML = names;
 }
