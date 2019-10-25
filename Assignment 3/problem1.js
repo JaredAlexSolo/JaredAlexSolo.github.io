1de3b36474431dcada16b604bd9079feef070121
JaredAlexSolo <jasolomon1@aggies.ncat.edu>
2019-10-24 22:35:59 -0400

    asdasd

--- Assignment 3/problem1.js
+++ Assignment 3/problem1.js
@@ -5,6 +5,7 @@ function loadName() {
   while ( x = window.prompt( "Value of x", 0 ) )  {
     document.getElementById('paragraph').innerHTML = "hey";
     names.push(parseInt(x));
+    y = window.prompt( "Value of y", 0 );
   }
-  document.getElementById('paragraph').innerHTML = names;
+  document.getElementById('paragraph').innerHTML = scores;
 }
