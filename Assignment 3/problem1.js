0f4326b405a3f5074a146eaf5a1ba3e00584bd10
JaredAlexSolo <jasolomon1@aggies.ncat.edu>
2019-10-24 21:21:36 -0400

    wasd

--- Assignment 3/problem1.js
+++ Assignment 3/problem1.js
@@ -1,10 +1,17 @@
 function loadName() {
-  var x = prompt("Yeet","Yeee");
+  var studentScores;
+  var studentName;
   var names = [];
   var scores = [];
 
-  while ( x = window.prompt( "Value of x", 0 ) )  {
-    names.push(parseInt(x));
+  while ( studentName = window.prompt(
+    "Student's name Cancel to Exit", John))  {
+    names.push(studentName);
+    studentScores = window.prompt( "Value of x", 0 );
+    scores.push(parseInt(studentScores));
   }
-  document.getElementById('paragraph').innerHTML = names;
+
+
+  document.getElementById('name').innerHTML = names;
+  document.getElementById('score').innerHTML = scores;
 }
