ddcc2280e1141d57c35c1160bbd41acff51fa41b
JaredAlexSolo <jasolomon1@aggies.ncat.edu>
2019-10-24 22:55:55 -0400

    asrfasdfdfas

--- Assignment 3/problem1.js
+++ Assignment 3/problem1.js
@@ -1,10 +1,12 @@
 function loadName() {
+  var minScore=0;
+  var maxScore=10;
   var names = [];
   var scores = [];
 
   while ( x = window.prompt( "Student's name Cancel to Exit", 0 ) )  {
     names.push(x);
-    y = window.prompt( "Student score(between 0 and 10)", 0 );
+    y = window.prompt( "Student score(between "+minScore+" and "+maxScore, 0 );
     scores.push(parseInt(y));
   }
   document.getElementById('p1').innerHTML = names;
